ee8fa5b3480f811247688a66ac1d37800f3c8e89
snsakthi <snithyasakthi@gmail.com>
2023-07-25 01:17:25 -0400

    added july25 file

--- src/myfirstproject/july25.java
+++ src/myfirstproject/july25.java
@@ -1,2 +1,7 @@
-package myfirstproject;public class july25 {
+package myfirstproject;
+
+public class july25 {
+    public static void main(String[] args) {
+        System.out.println("Hello Everyone");
+    }
 }
